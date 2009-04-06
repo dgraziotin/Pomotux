259a9ae896a021d88dcd32830cfebfad884b8f1e
rbuttarelli <rbuttarelli@bfc077dc-68ba-492c-b29a-357650234e15>
2009-04-06 16:59:51 +0000

    compile and run script updated for the new files
    git-svn-id: https://babbage.inf.unibz.it/teaching/2009/sep/team8/trunk@33 bfc077dc-68ba-492c-b29a-357650234e15

--- src/compileandrun.sh
+++ src/compileandrun.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 litesql-gen pomotuxdatabase.xml
-g++ pomotuxdatabase.cpp main.cpp -llitesql -o pomotux
+g++ pomotuxdatabase.cpp main.cpp iactivity.cpp iactivityinventorysheet.cpp itodotodaysheet.cpp -llitesql -o pomotux
 ./pomotux
