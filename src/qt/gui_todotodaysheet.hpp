fb738caf0793e3cc3df77217a8fcbff556a46bc0
dgraziotin <dgraziotin@bfc077dc-68ba-492c-b29a-357650234e15>
2009-04-29 17:10:21 +0000

    Merge of branch "integration" into trunk
    git-svn-id: https://babbage.inf.unibz.it/teaching/2009/sep/team8/trunk@87 bfc077dc-68ba-492c-b29a-357650234e15

---
+++ src/qt/gui_todotodaysheet.hpp
@@ -0,0 +1,41 @@
+#ifndef TODOTODAYSHEETGUI_H
+#define TODOTODAYSHEETGUI_H
+
+#include <QtGui/QMainWindow>
+#include <litesql.hpp>
+#include "../pomotuxdatabase.hpp"
+#include "pomodoro.hpp"
+
+using namespace litesql;
+using namespace pomotuxdatabase;
+using namespace std;
+
+namespace Ui {
+class TodoTodaySheetGuiClass;
+}
+
+class TodoTodaySheetGui : public QMainWindow {
+    Q_OBJECT
+
+public:
+    TodoTodaySheetGui(QWidget *parent,PomotuxDatabase& database);
+    ~TodoTodaySheetGui();
+
+
+private:
+    PomotuxDatabase *db;
+    Pomodoro *pomo;
+    Activity *current;
+    Ui::TodoTodaySheetGuiClass *ui;
+
+    void cleaner();
+    void refreshTable();
+
+private slots:
+    void on_stopActivity_clicked();
+    void on_postponeActivityButton_clicked();
+    void on_startActivityButton_clicked();
+    void on_finishActivityButton_clicked();
+};
+
+#endif // TODOTODAYSHEETGUI_H
---
+++ src/qt/modifyactivity.cpp
@@ -0,0 +1,39 @@
+#include "modifyactivity.hpp"
+#include "ui_modifyanactivity.h"
+
+ModifyAnActivity::ModifyAnActivity(QWidget *parent, PomotuxDatabase& database) :
+        QDialog(parent),
+        m_ui(new Ui::ModifyAnActivity)
+{
+    db = &database;
+    m_ui->setupUi(this);
+}
+
+ModifyAnActivity::~ModifyAnActivity()
+{
+    delete m_ui;
+}
+
+void ModifyAnActivity::changeEvent(QEvent *e)
+{
+    QDialog::changeEvent(e);
+    switch (e->type()) {
+    case QEvent::LanguageChange:
+        m_ui->retranslateUi(this);
+        break;
+    default:
+        break;
+    }
+}
+
+void ModifyAnActivity::on_buttonBox_accepted()
+{
+    text = this->m_ui->mADescriptionLineEdit->text();
+    dayToDeadline = this->m_ui->mADeadlineSpinBox->value();
+    this->close();
+}
+
+void ModifyAnActivity::on_buttonBox_rejected()
+{
+    this->close();
+}
