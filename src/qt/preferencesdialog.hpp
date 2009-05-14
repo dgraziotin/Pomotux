35a6b9d6de5f31d19e2564da02d59993ea763b5e
rbuttarelli <rbuttarelli@bfc077dc-68ba-492c-b29a-357650234e15>
2009-05-14 09:07:56 +0000

    doxygen for preferences dialog
    git-svn-id: https://babbage.inf.unibz.it/teaching/2009/sep/team8/trunk@168 bfc077dc-68ba-492c-b29a-357650234e15

--- src/qt/preferencesdialog.hpp
+++ src/qt/preferencesdialog.hpp
@@ -16,11 +16,33 @@ namespace Ui {
 class PreferencesDialog;
 }
 
+/**
+  * A class for Preference Dialog Graphical User Interface
+  * It Contains all the operations and representations of data necessary to let the final user sets the program settings.
+  * Preference dialog exanchanges signals with the Todo Today Sheet Window in order to maintain the consistency with user choices
+  * and how tasks are actually carried out.
+  * This class depends on several libraries: QtGui/QDialog and litesql libraries.
+  * This class uses the taylor-made class PomotuxException to handle internal Exceptions whenever it become necessary
+  *
+  * @see PomotuxExcetion.hpp
+  * @see PomotuxDatabase.hpp
+  * @see http://apps.sourceforge.net/trac/litesql/
+  */
 class PreferencesDialog : public QDialog {
     Q_OBJECT
     Q_DISABLE_COPY(PreferencesDialog)
 public:
+
+    /**
+      * Window Constructor
+      * @param parent a pointer to the parent widget
+      * @param database a reference to the PomotuxDatabase database in use
+      */
     explicit PreferencesDialog(QWidget *parent,PomotuxDatabase& database);
+
+    /**
+      * Default Destructor
+      */
     virtual ~PreferencesDialog();
 
 protected:
@@ -28,12 +50,28 @@ protected:
 
 private:
     Ui::PreferencesDialog *m_ui;
+
+    /**
+      * A pointer to the litesql Database in use
+      */
     PomotuxDatabase *mpDatabase;
 
 private slots:
+
+    /**
+      * Rejects all the changes made to Settings it simply hide the window and nothing will be sent to the database
+      */
     void on_buttonBox_rejected();
+
+    /**
+      * Confirms the changes made to Settings and sends the new settings to the database
+      */
     void on_buttonBox_accepted();
 signals:
+
+    /**
+      * Signal Emitted everytime preferences are updated
+      */
     void DatabaseUpdated();
 };
 
