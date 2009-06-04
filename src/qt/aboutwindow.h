3e33591eff5f1b31388a4ce68c7c6fa94e1a9547
mpergher <mpergher@bfc077dc-68ba-492c-b29a-357650234e15>
2009-06-04 11:34:10 +0000

    doxy for the aboutwindow class
    git-svn-id: https://babbage.inf.unibz.it/teaching/2009/sep/team8/trunk@195 bfc077dc-68ba-492c-b29a-357650234e15

--- src/qt/aboutwindow.h
+++ src/qt/aboutwindow.h
@@ -1,3 +1,11 @@
+/**
+ * A class for About window.
+ * It shows to the user some information about the program and about the programmer
+ *
+ *  @include <QtGui/QDialog>
+ *
+ *  @see http://doc.trolltech.com/4.5/index.html
+ */
 #ifndef ABOUTWINDOW_H
 #define ABOUTWINDOW_H
 
@@ -7,17 +15,35 @@ namespace Ui {
     class AboutWindow;
 }
 
+/**
+ * A class for About window.
+ * It shows to the user some information about the program and about the programmer
+ *
+ *  @see http://doc.trolltech.com/4.5/index.html
+ */
 class AboutWindow : public QDialog {
     Q_OBJECT
     Q_DISABLE_COPY(AboutWindow)
 public:
+
+    /**
+     * Constructor of a About Graphical User Interface window.
+     * @param parent a pointer to the parent widget (0 if we want a new window)
+     */
     explicit AboutWindow(QWidget *parent = 0);
+
+    /**
+     * Default Destructor
+     */
     virtual ~AboutWindow();
 
 protected:
     virtual void changeEvent(QEvent *e);
 
 private:
+    /**
+     * Default pointer created by qtCreator
+     */
     Ui::AboutWindow *m_ui;
 };
 
